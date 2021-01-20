619a57d46f7e7d09766142186f46b3078e14314a
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-01-20 10:49:50 +0700

    readme

--- app/src/main/java/com/gzeinnumer/mylibsqlitebuilderexample/table/Table1.java
+++ app/src/main/java/com/gzeinnumer/mylibsqlitebuilderexample/table/Table1.java
@@ -3,7 +3,7 @@ package com.gzeinnumer.mylibsqlitebuilderexample.table;
 
 import com.gzeinnumer.mylibsqlitebuilder.struct.CreateTableQuery;
 
-@CreateTableQuery
+@CreateTableQuery(query = "CREATE TABLE table1 (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, rating REAL, descr TEXT, flag_active INTEGER, created_at TEXT)")
 public class Table1 {
     private int id;
     private String name;
